6ba889d7fec4af291f66d85104759bc4e7e84299
Ferreyra, Maximiliano Gastón <ferreyra.mg.utnfrba@gmail.com>
2015-05-23 15:13:02 -0300

    Ejercicio terminado

---
+++ Modulo 1/Estructuras Secuenciales/M1-1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b, suma, dif, prod;
+
+    cout << "Ingrese un numero entero: " << endl;
+    cin >> a;
+    cout << "Ingrese un numero entero: " << endl;
+    cin >> b;
+
+    suma = a + b;
+
+    dif = a - b;
+
+    prod = a * b;
+
+    cout << "La suma de ambos es: " << suma << endl;
+
+    cout << "La diferencia entre ambos es: " << dif << endl;
+
+    cout << "El producto de ambos es: " << prod << endl;
+
+}
